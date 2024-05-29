fdbbe2f2b364a1f878e13666c534c40998e2302c
asad4405 <asadrifat450@gmail.com>
2024-05-29 22:43:12 +0600

    add

--- resources/views/layouts/backend_master.blade.php
+++ resources/views/layouts/backend_master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Dashboard - NiceAdmin Bootstrap Template</title>
+    <title>Dashboard - </title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
@@ -30,14 +30,6 @@
 
     <!-- Template Main CSS File -->
     <link href="{{ asset('backend_assets') }}/css/style.css" rel="stylesheet">
-
-    <!-- =======================================================
-  * Template Name: NiceAdmin
-  * Updated: Mar 09 2023 with Bootstrap v5.2.3
-  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 </head>
 
 <body>
